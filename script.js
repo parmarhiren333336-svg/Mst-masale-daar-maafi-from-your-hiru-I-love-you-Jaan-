4934f1e59bdb9f7a4d0dc9825e65d8ea29967d3d
parmarhiren333336-svg <parmarhiren333336@gmail.com>
2026-07-08 20:12:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,74 @@
-// Script Coming Soon
+window.onload = function(){
+
+    setTimeout(function(){
+
+        document.getElementById("loading").style.display="none";
+
+        document.getElementById("galaxy").style.display="flex";
+
+        startTyping();
+
+    },3000);
+
+};
+
+
+
+function startTyping(){
+
+    let text = "Mahi ❤️ You are the most special person in my life ✨";
+
+    let i = 0;
+
+    let speed = 80;
+
+
+    function type(){
+
+        if(i < text.length){
+
+            document.getElementById("typing").innerHTML += text.charAt(i);
+
+            i++;
+
+            setTimeout(type,speed);
+
+        }
+
+    }
+
+
+    type();
+
+}
+
+
+
+
+function startJourney(){
+
+    document.getElementById("galaxy").style.display="none";
+
+    document.getElementById("giftBox").style.display="flex";
+
+}
+
+
+
+
+function openGift(){
+
+    document.getElementById("giftBox").style.display="none";
+
+    document.getElementById("letter").style.display="block";
+
+}
+
+
+
+
+function complete(){
+
+    alert("Our Journey Begins Forever ❤️");
+
+}
